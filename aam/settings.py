b64691b5e1d90949edc133786daac060557f7865
hermantoXYZ <andiupdates@gmail.com>
2026-01-28 14:08:02 +0800

    p

--- aam/settings.py
+++ aam/settings.py
@@ -86,11 +86,11 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_successss',  # Ganti dengan nama database yang Anda buat di phpMyAdmin /bisdigun_app_success
+        'NAME': 'esuh2918_db_success',  # Ganti dengan nama database yang Anda buat di phpMyAdmin /bisdigun_app_success
         'USER': 'root',  # Nama pengguna MySQL, biasanya 'root' di XAMPP /bisdigun_app_root
-        'PASSWORD': '',  # Kata sandi pengguna MySQL, jika tidak ada, biarkan kosong
-        'HOST': 'localhost',  # Host server database, localhost untuk XAMPP
-        'PORT': '3307',  # Port MySQL (default 3306)
+        'PASSWORD': 'root',  # Kata sandi pengguna MySQL, jika tidak ada, biarkan kosong
+        'HOST': '127.0.0.1',  # Host server database, localhost untuk XAMPP
+        'PORT': '3310',  # Port MySQL (default 3306)
     }
 }
 
